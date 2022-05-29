9e613afbcd670b941c8d10e6a8881d8431907ad6
wa101 <wadhah.mahroug15@gmail.com>
2022-05-30 00:49:30 +0100

    added functions

--- GUI/get_image_frame.py
+++ GUI/get_image_frame.py
@@ -60,6 +60,43 @@ class UploadFileFrame(tk.Frame):
         canvas.draw()
         canvas.get_tk_widget().grid(row=2, column=1)
 
+    def linear_transform(self):
+        img = self.image
+        lin_img = (img - img.min()) / (img.max() - img.min())
+        lin_img = lin_img * 255
+        self.set_image(lin_img)
+
+    def equalize(self):
+        equ = cv2.equalizeHist(self.image)
+        self.set_image(equ)
+
+    def apply_noise(self):
+        flat_noisy_image = np.array([rand_helper(i)
+                                    for i in self.image.image.flatten()])
+        a = flat_noisy_image.reshape(self.image.image.shape)
+        self.set_image(a)
+
+    def mean_filter(self, w=1):
+        image = self.image
+        filtered = image.copy()
+        for i in range(w, image.shape[0]-w):
+            for j in range(w, image.shape[1]-w):
+                block = image[i-w:i+w+1, j-w:j+w+1]
+                mean_result = np.mean(block, dtype=np.float32)
+                filtered[i][j] = int(mean_result)
+        self.set_image(filtered)
+
+    def median_filter(self, w=1):
+        image = self.image
+        filtered = self.copy()
+        for i in range(w, image.shape[0]-w):
+            for j in range(w, image.shape[1]-w):
+                block: np.ndarray = image[i-w:i+w+1, j-w:j+w+1]
+                flat_sorted_block = np.sort(block.flatten())
+                median_value = flat_sorted_block[4]
+                filtered[i][j] = int(median_value)
+        self.set_image(filtered)
+
 
 # root = tk.Tk()
 # hc = UploadFileFrame(root)
