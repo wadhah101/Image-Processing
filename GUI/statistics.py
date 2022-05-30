9afa0a545286cf5707fe99bfe8e9784108a976f5
Tess99854 <stesnime@yahoo.com>
2022-05-30 10:34:52 +0100

    sth

--- GUI/statistics.py
+++ GUI/statistics.py
@@ -4,13 +4,30 @@ import numpy as np
 
 
 class StatisticsFrame(Frame):
+    def get_image_average(self):
+        return np.average(self.image)
+
+    def get_standard_deviation(self):
+        return np.std(self.image)
+
     def __init__(self, parent, image: np.ndarray):
         super().__init__(parent)
 
-        footer = LabelFrame(self, font=('Raleway', 25), width=1200,
-                            height=100, bg='#3b3b3b', borderwidth=0)
+        footer = LabelFrame(self, font=('Raleway', 25), bg='#3b3b3b', borderwidth=0)
         footer.grid(column=0)
 
+        right_stat = LabelFrame(footer)
+        right_stat.grid(row=0, column=0, sticky=W)
 
+        average = Label(right_stat, text='0.00')
+        average.grid(row=0)
 
+        std = Label(right_stat, text='0.00')
+        std.grid(row=1)
+
+        left_stat = LabelFrame(footer)
+        left_stat.grid(row=0, column=1, sticky=E)
+
+        resolution = Label(left_stat, text='000x000')
+        resolution.grid(row=0)
 
