3d35c29e15469eba7f04c0c1b51dbf007d2f93ef
Tess99854 <stesnime@yahoo.com>
2022-05-30 09:42:35 +0100

    and or buttons

--- GUI/segmentation_utils_frame.py
+++ GUI/segmentation_utils_frame.py
@@ -1,6 +1,6 @@
 import random
 import tkinter
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter.messagebox import showinfo
@@ -14,21 +14,36 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 from matplotlib.figure import Figure
 
 
-class SegmentationUtilsFrame(tk.Frame):
+class SegmentationUtilsFrame(Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.variable = None
         self.create_widgets()
 
     def create_widgets(self):
         # sliders
-        self.r_slider = tk.Scale(self, from_=0, to=256, orient=tk.HORIZONTAL)
+        self.r_slider = Scale(self, from_=0, to=256, orient=HORIZONTAL)
         self.r_slider.grid(row=1)
-        self.g_slider = tk.Scale(self, from_=0, to=256, orient=tk.HORIZONTAL)
+        self.g_slider = Scale(self, from_=0, to=256, orient=HORIZONTAL)
         self.g_slider.grid(row=2)
 
-        self.b_slider = tk.Scale(self, from_=0, to=256, orient=tk.HORIZONTAL)
+        self.b_slider = Scale(self, from_=0, to=256, orient=HORIZONTAL)
         self.b_slider.grid(row=3)
+
+        switch_frame = LabelFrame(self)
+        switch_frame.grid(row=4)
+
+        self.variable = StringVar(None, "AND")
+
+        AND_button = Radiobutton(switch_frame, text="AND", variable=self.variable,
+                                 indicatoron=False, value="AND", width=8)
+        OR_button = Radiobutton(switch_frame, text="OR", variable=self.variable,
+                                indicatoron=False, value="OR", width=8)
+
+        AND_button.grid(row=0, column=0)
+        OR_button.grid(row=0, column=1)
+
 
         # def uploadFileCommand(self):
         #     self.filename = fd.askopenfilename(
@@ -46,7 +61,6 @@ class SegmentationUtilsFrame(tk.Frame):
         #     canvas = FigureCanvasTkAgg(fig, master=self)  # A tk.DrawingArea.
         #     canvas.draw()
         #     canvas.get_tk_widget().grid(row=2, column=1)
-
 
 # root = tk.Tk()
 # hc = SegmentationUtilsFrame(root)
